f975386cc9bfd1930e57f5f3836f20520e4cc09b
BulbFreak <saldarriagai@ufl.edu>
2021-10-19 14:21:27 -0400

    Initial commit

---
+++ src/Hello.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Hello {
+    public static void main(String[] args) {
+
+        // Creating scanner for user input
+        Scanner scnr = new Scanner(System.in);
+        // Pre-expressing all my scanner variables for organization purposes
+        String name;
+        String age;
+        String home;
+
+        /* Print question without a 'new line' so that the answer is pasted to the
+            right of the question.*/
+        System.out.print("Hello. What is your name? ");
+        /* The next code allows the user to input their 'name' and so on, using the
+            previously established scanner variables */
+        name = scnr.next();
+        System.out.print("It's nice to meet you, " + name + ". How old are you? ");
+        age = scnr.next();
+        System.out.println("I see that you are still quite young at only " + age + ".");
+        System.out.print("Where do you live? ");
+        home = scnr.next();
+        System.out.println("Wow! I've always wanted to go to " + home + ". Thanks for chatting with me. Bye!");
+    }
+}
